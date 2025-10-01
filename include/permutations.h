9183a4bf8ccad1fe095bfe54f814a7427a03d223
anastasiail <anastasiailasova0@gmail.com>
2025-10-01 22:00:48 +0300

    Update permutations.h

--- include/permutations.h
+++ include/permutations.h
@@ -3,26 +3,15 @@
 
 #include <stdlib.h>
 
-typedef struct Permutation {
-    unsigned *nums;
-    unsigned n;
-} permutation_t;
+typedef struct Permutation permutation_t;
+typedef struct PermutationIterator iterator_t;
 
-typedef struct PermutationIterator {
-    permutation_t value;
-    int next_perm_exist;
-} iterator_t;
-
+// Объявления функций
 void iterator_init(iterator_t *i, unsigned n);
-
 const permutation_t *iterator_value(const iterator_t *i);
-
 int iterator_has_next(const iterator_t *i);
-
 void iterator_next(iterator_t *i);
-
 void iterator_destroy(iterator_t *i);
-
 void print_permutation(const permutation_t *perm);
 
 #endif
